16dbd01c9dc46badd1c8d1caf393fd925f0388f6
Jodie <jlo1995@hotmail.com>
2021-02-20 19:10:25 +0000

    now only reads certain IDs

--- Read.py
+++ Read.py
@@ -1,12 +1,16 @@
 import RPi.GPIO as GPIO
 import pdb
 from mfrc522 import SimpleMFRC522
+from MagicBandIds import ids as magic_band_ids
 
 reader = SimpleMFRC522()
 
 try:
 	data = reader.read()
-	print("The ID is: ", data[0])
+	read_id = data[0]
+	
+	if read_id in magic_band_ids.values():
+		print("The ID is: ", read_id)
 
 finally:
 	GPIO.cleanup()
