5a9fc7cd56884f47bfcb1783f475b4c963ec4ebc
Jodie <jlo1995@hotmail.com>
2021-03-23 22:37:01 +0000

    Can connect to lights

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# MagicBandReader
-# ID: 
+ 
--- Read.py
+++ Read.py
@@ -1,9 +1,14 @@
 import RPi.GPIO as GPIO
 import pdb
+import board
+import neopixel
 from mfrc522 import SimpleMFRC522
 from MagicBandIds import ids as magic_band_ids
 
 reader = SimpleMFRC522()
+pixels = neopixel.NeoPixel(board.D18, 50, auto_write=False)
+pixels.fill((0, 255, 0))
+pixels.show()
 
 try:
 	data = reader.read()
